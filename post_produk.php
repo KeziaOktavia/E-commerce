ad3731b93bbc23d4eefc04f5d83d0d1bf8df8aaa
TriIntanSyarifah1011 <intansyarifah2023@gmail.com>
2025-05-08 19:50:30 +0700

    Menambahkan tampilan produk di post_produk.php

--- post_produk.php
+++ post_produk.php
@@ -11,32 +11,37 @@ include '.includes/header_admin.php';
         <div class="card-body">
             <form method="POST" action="proses_post.php"
             enctype="multipart/form-data">
-<!-- Input untuk Nama Produk -->
- <div class="mb-3">
-    <label for="produk_name" class="form-label">Nama Produk</label>
-    <input type="text"  class="form-control"name="produk_name" required>
-</div>
-<!-- Input untuk mengunggah gambar -->
+
+            <!-- Input untuk mengunggah gambar -->
  <div class="mb-3">
     <label for="formFile" class="form-label">Unggah Gambar</label>
     <input class="form-control" type="file" name="image" accept="image/*" />
 </div>
+<!-- Input untuk Nama Produk -->
+ <div class="mb-3">
+    <label for="namaProduk" class="form-label">Nama Produk</label>
+    <input type="text"  class="form-control"name="namaProduk" required>
+</div>
+
 <!-- Input untuk Harga Produk -->
 <div class="mb-3">
     <label for="harga" class="form-label">Harga Satuan</label>
     <input type="text"  class="form-control"name="harga" required>
 </div>
+
 <!-- Input untuk Stok Produk -->
 <div class="mb-3">
     <label for="stok" class="form-label">Stok</label>
     <input type="number"  class="form-control"name="stok" required>
 </div>
+
 <!-- Dropdown untuk memilih kategori -->
- <div class="mb-3">
+<div class="mb-3">
     <label for="kategori_id" class="form-label">Kategori</label>
     <select class="form-select" name="kategori_id" required>
 <!-- Mengambil data kategori dari database untuk mengisi opsi dropdown -->
- <option value="" selected disabled>Pilih salah satu</option>
+<option value="" selected disabled>Pilih salah satu</option>
+
  <?php
  $query = "SELECT * FROM kategori"; // Query untuk mengambil data kategori
  $result = $conn->query($query); // Menjalankan query 
@@ -48,12 +53,6 @@ include '.includes/header_admin.php';
 ?>
 </select>
 </div>
-<!-- Textarea untuk deskripsi Produk -->
-<div class="mb-3">
-    <label for="content" class="form-label">Deskripsi</label>
-    <textarea class="form-control" id="content" name="deskripsi" required></textarea>
-</div>
-
 <!-- Tombol submit -->
  <button type="submit" name="simpan" class="btn btn-primary">Posting Produk</button>
 </form>
@@ -62,7 +61,7 @@ include '.includes/header_admin.php';
 </div>
 </div>
 </div>
-<?php
+<?
 // Menyertakan footer halaman
 include '.includes/footer.php';
 ?>
