27fe85e8376679a2df8f4af295565a65d567828b
nurulfadhilah05 <nurul.fadhilah1078@smk.belajar.id>
2025-05-08 14:05:55 +0700

    memperbarui register_proses.php

--- auth/register_proses.php
+++ auth/register_proses.php
@@ -3,15 +3,18 @@ require_once("../config.php");
 // Mulai session
 session_start();
 
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST["username"];
-    $nama = $_POST["nama"];
+    $name = $_POST["nama"];
     $email = $_POST["email"];
     $alamat = $_POST["alamat"];
     $password = $_POST["password"];
     $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
 
-    $sql = "INSERT INTO pelanggan (username, nama, email, alamat, password) VALUES ('$username','$nama','$email','$alamat','$hashedPassword')";
+    $role = 'pelanggan';
+
+    $sql = "INSERT INTO pelanggan (username, nama, email, alamat, password, role) VALUES ('$username','$name','$email','$alamat','$hashedPassword','$role')";
     if ($conn->query($sql) == TRUE) {
         // Simpan notifikasi ke dalam session
         $_SESSION['notification'] = [
