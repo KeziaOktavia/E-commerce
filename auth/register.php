7187fecc6919baa7e2b125b4db8796ba44953975
TriIntanSyarifah1011 <intansyarifah2023@gmail.com>
2025-05-24 09:14:39 +0700

    Melanjutkan tampilan register

--- auth/register.php
+++ auth/register.php
@@ -6,27 +6,28 @@
     <div class="app-brand justify-content-center">
       <a href="index.html" class="app-brand-link gap-2">
         <span class="app-brand-logo demo"></span>
-        <span class="app-brand-text demo text-uppercase fw-bolder">E-commerce</span>
+        <span class="app-brand-text demo text-uppercase fw-bolder">SHOMERCE</span>
       </a>
     </div>
     <!-- /Logo -->
-    <form action="register_process.php" class="mb-3" method="POST">
-    <div class="mb-3">
-        <label for="nama" class="form-label">Nama</label>
-        <input type="text" class="form-control" name="nama" placeholder="Masukkan Nama" />
-      </div>
-      <div class="mb-3">
-        <label for="email" class="form-label">Email</label>
-        <input type="email" class="form-control" name="email" placeholder="Masukkan Email" />
-      </div>
+    <form action="register_proses.php" class="mb-3" method="POST">
       <div class="mb-3">
         <label for="username" class="form-label">Username</label>
         <input type="text" class="form-control" name="username" placeholder="Masukkan Username" autofocus/>
       </div>
       <div class="mb-3">
-        <label for="alamat" class="form-label">Alamat</label>
-        <input type="text" class="form-control" name="alamat" placeholder="Masukkan alamat" />
+        <label for="email" class="form-label">Nama</label>
+        <input type="text" class="form-control" name="nama" placeholder="Masukkan Nama" />
       </div>
+      <div class="mb-3">
+    <label for="email" class="form-label">Email</label>
+    <input type="email" class="form-control" name="email" placeholder="Masukkan Email" required />
+  </div>
+
+  <div class="mb-3">
+    <label for="alamat" class="form-label">Alamat</label>
+    <textarea class="form-control" name="alamat" rows="3" placeholder="Masukkan Alamat Lengkap" required></textarea>
+  </div>
       <div class="mb-3 form-password-toggle">
         <label class="form-label" for="password">Password</label>
         <div class="input-group input-group-merge">
